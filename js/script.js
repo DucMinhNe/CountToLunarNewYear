22030675e2bdeb33f6780c5e8563d0a6bb17d438
Minh Le Duc <minh.leduc.lt@gmail.com>
2024-07-23 23:17:19 +0700

    update 2025

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const tetDate = new Date("2024-02-10T00:00:00Z");
+    const tetDate = new Date("2025-01-29T00:00:00Z");
 
     function updateCountdown() {
         const currentDate = new Date();
         const timeDifference = tetDate - currentDate;
 
         if (timeDifference <= 0) {
-            document.getElementById("countdown").innerHTML = "Chúc mừng năm mới 2024!";
+            document.getElementById("countdown").innerHTML = "Chúc mừng năm mới 2025!";
         } else {
             const daysRemaining = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
             document.getElementById("countdown").innerHTML = `Còn ${daysRemaining} ngày nữa là đến Tết Nguyên đán`;
